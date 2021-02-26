ea5a41ab71ff75da12e8f9ffd7e7bc6198a8fa94
“JeffersonAFranca” <jefferson.amorim.franca@gmail.com>
2021-02-26 09:05:22 -0300

    Server creation and route testing with insomnia

--- src/server.ts
+++ src/server.ts
@@ -2,4 +2,40 @@ import express from 'express';
 
 const app = express();
 
+/*
+ROTAS: São recursos disponíveis dentro de uma API
+Parâmetros de uma rota (request, response)  
+
+METODOS: Alguns que poderão ser utilizados na API 
+
+  -GET    => Busca
+  -POST   => salvar
+  -PUT    => Alterar
+  -DELETE => Deletar
+  -PATCH  => Alteração específicas
+
+
+    GET: Função que recebe uma rota 
+    
+    No exemplo da rota ("/",) o rota está sendo direcionada para pasta raiz
+        app.get("/", (request, response) =>{
+            return response.send("Hello Word!!")
+        Exemplo: http://localhost:3333/
+    
+    No exemplo da rota "/users" utilizando o parâmetro GET é onde está os recursos para acessados     
+        app.get("/users", (request, response) =>{
+            return response.send("Hello Word!!")
+        Exemplo: http://localhost:3333/users
+*/
+app.get("/", (request, response) =>{
+    return response.json({message: "Hello Word!!"})
+})
+
+app.post("/", (request, response) =>{
+    //Recebeu os dados para salvar
+    return response.json({message: "Os dados foram salvos com sucesso!"})
+})
+
+
+ // Iniciando Servidor
 app.listen(3333, () => console.log('Server is running') );
