9504fad3e96280bf6808ebc9faf85dcf68e40e50
“JeffersonAFranca” <jefferson.amorim.franca@gmail.com>
2021-02-27 21:48:26 -0300

    Final class-02: Migration User / Model User / Controller User/ Roouter User: Restore all project files

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import 'reflect-metadata';
 import express, { request, response } from 'express';
-import "./database"; //Importando arquivo de conexão com o database - Devido o arquivo de estar com nome "index" , não é necessário informar o nome do arquivo. 
+import "./database";  //Importando arquivo de conexão com o database - Devido o arquivo de estar com nome "index" , não é necessário informar o nome do arquivo. 
 import { router } from "./routes";
 
 const app = express();
