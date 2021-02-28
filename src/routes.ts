0a2fe139a50144a3e90b09a3322054bdee89421a
“JeffersonAFranca” <jefferson.amorim.franca@gmail.com>
2021-02-28 00:13:43 -0300

    Completed search inclusion: Controllers / Routes / Repository | Tests OK

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 
 //Importação do "Router" de dentro do "express" 
 import { Router} from 'express';
+import { SurveyController } from './controllers/SurveysController';
 //Importando a class UserController para dentro do documento.
 import { UserController } from "./controllers/UserController";
 
@@ -8,7 +9,9 @@ const router = Router()
 
 //Instânciando class para poder ser utilizada dentro do documento
 const userController = new UserController();
+const surveyController = new SurveyController()
 
-router.post("/users", userController.create);
+router.post("/users"  , userController.create);
+router.post("/surveys", surveyController.create);
 
 export { router }
