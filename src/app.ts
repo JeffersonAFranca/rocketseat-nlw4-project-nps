cf056073b4f5dfe14d736fa0d4fe373019093bd5
“JeffersonAFranca” <jefferson.amorim.franca@gmail.com>
2021-03-02 21:43:51 -0300

    Fixed server connection issue | Tests: OK!

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
 import 'reflect-metadata';
-import express from 'express';
-import './database'; 
-import { router } from "./routes";
+import express             from 'express';
+import createConnection    from './database'; 
+import { router }          from "./routes";
 
-
+createConnection();
 const app = express();
 
 app.use(express.json()); // Faz com que o express receba aquivos json
